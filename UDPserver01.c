07cdfeb1f5690ad778cb9edc19c6e07fbe412c20
holly204 <115499072+holly204@users.noreply.github.com>
2022-11-10 11:54:19 -0800

    Add files via upload

--- UDPserver01.c
+++ UDPserver01.c
@@ -32,6 +32,35 @@ Student ID: W1641460
 #define REJECT_DATA_MISSING  0XFFF6
 #define REJECT_DUPLICATE 0XFFF7
 
+//structure of Data Packet
+struct DataPacket{
+	unit16_t StartPacketId;
+	unit8_t ClientId;
+	unit16_t Data;
+	unit8_t SegmentNo;
+	unit8_t Length;
+	unsigned int Payload;
+	unit16_t EndPacketId;	
+}
+
+//Structure of ACK packet
+struct ACKPacket{
+	unit16_t StartPacketId;
+	unit8_t ClientId;
+	unit16_t ACK;
+	unit8_t ReceivedSegmentNo;
+	unit16_t EndPacketId;
+}
+//structure of Reject Pakcet
+struct RejectPacket{
+	unit16_t StartPacketId;
+	unit8_t ClientId;
+	unit16_t Reject;
+	unit16_t Reject_sub_code;
+	unit8_t ReceivedSegmentNo;
+	unit16 EndPacketId;
+}
+
 
 int main(int argc, char **argv)
 {
